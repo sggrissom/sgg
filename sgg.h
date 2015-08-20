1174af463c1104571c931e3b83d0f867d107aae4
Steven Grissom <sggrissom@gmail.com>
2015-08-20 12:53:56 -0500

    add compiler macro variables

--- sgg.h
+++ sgg.h
@@ -10,10 +10,36 @@
 extern "C" {
 #endif
 
+#if !defined(COMPILER_MSVC)
+#define COMPILER_MSVC 0
+#endif
+    
+#if !defined(COMPILER_LLVM)
+#define COMPILER_LLVM 0
+#endif
+
+#if !COMPILER_MSVC && !COMPILER_LLVM
+#if _MSC_VER
+#undef COMPILER_MSVC
+#define COMPILER_MSVC 1
+#else
+#undef COMPILER_LLVM
+#define COMPILER_LLVM 1
+#endif
+#endif
+
+#if COMPILER_MSVC
+#include <intrin.h>
+#endif
+
 #include <stdint.h>
 
+    typedef int8_t s8;
+    typedef int16_t s16;
     typedef int32_t s32;
     typedef int64_t s64;
+    typedef uint8_t u8;
+    typedef uint16_t u16;
     typedef uint32_t u32;
     typedef uint64_t u64;
     typedef float r32;
@@ -28,18 +54,18 @@ extern "C" {
 
 #if SLOW
 // TODO(steven): Complete assertion macro
-#define assert(x) if(!(x)) {*(int *)0 = 0;}
+#define Assert(x) if(!(x)) {*(int *)0 = 0;}
 #else
-#define assert(x)
+#define Assert(x)
 #endif
 
-#define InvalidCodePath assert(!"InvalidCodePath")
+#define InvalidCodePath Assert(!"InvalidCodePath")
 #define InvalidDefaultCase default: {InvalidCodePath;} break
 
-#define kilobytes(value) ((value)*1024LL)
-#define megabytes(value) (kilobytes(value)*1024LL)
-#define gigabytes(value) (megabytes(value)*1024LL)
-#define terabytes(value) (gigabytes(value)*1024LL)
+#define Kilobytes(value) ((value)*1024LL)
+#define Megabytes(value) (Kilobytes(value)*1024LL)
+#define Gigabytes(value) (Megabytes(value)*1024LL)
+#define Terabytes(value) (Gigabytes(value)*1024LL)
 
 #define ArrayCount(array) (sizeof(array) / sizeof((array)[0]))
 #define Minimum(x,y) ((x) < (y) ? (x) : (y))

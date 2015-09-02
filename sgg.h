422b8f4bf6ec48134094a2a07b2112dcfffe006e
Steven Grissom <sggrissom@gmail.com>
2015-09-02 14:05:40 -0500

    adding a auto resizeable buffer for quick dev in c. Analog to c++ vector. Sourced from stb_stretchy buffer (woohoo nothings/sean barrett

--- sgg.h
+++ sgg.h
@@ -54,7 +54,7 @@ extern "C" {
 
 #if SLOW
 // TODO(steven): Complete assertion macro
-#define Assert(x) if(!(x)) {*(int *)0 = 0;}
+#define Assert(x) if(!(x)) {*(volatile u32 *)0 = 0;}
 #else
 #define Assert(x)
 #endif
@@ -72,6 +72,61 @@ extern "C" {
 #define Maximum(x,y) ((x) > (y) ? (x) : (y))
 // TODO(steven): swap. maybe xor swap?
 
+internal u32
+sg_atoi(const char *s)
+{
+    u32 result = 0;
+
+    while(*s)
+    {
+        result <<= 1;
+        result += (result << 2);
+        result += (*s++ & 0x0f);
+    }
+
+    return result;
+}
+
+#define buffer_free(a)       ((a) ? free(buffer_raw(a)),0 : 0)
+#define buffer_push(a,v)     (buffer_maybe_grow(a,1), (a)[buffer_used(a)++] = (v))
+#define buffer_count(a)      ((a) ? buffer_used(a) : 0)
+#define buffer_grow(a,n)     (buffer_maybe_grow(a,n), buffer_used(a)+=(n),&(a)[buffer_used(a)-(n)])
+#define buffer_last(a)       ((a)[buffer_used(a)-1])
+
+#define buffer_raw(a)        ((s32 *) (a) - 2)
+#define buffer_size(a)       buffer_raw(a)[0]
+#define buffer_used(a)       buffer_raw(a)[1]
+
+#define buffer_test(a,n)     ((a)==0 || buffer_used(a)+(n) >= buffer_size(a))
+#define buffer_maybe_grow(a,n) (buffer_test(a,(n)) ? buffer_expand(a,n) : 0)
+#define buffer_expand(a,n)   ((a) = sg_grow_buffer((a), (n), sizeof(*(a))))
+    
+internal void *
+sg_grow_buffer(void *Array, s32 SizeIncrease, u32 ItemSize)
+{
+    void *Result = 0;
+    
+    s32 DoubleCurrentSize = Array ? 2*buffer_size(Array) : 0;
+    u32 MinimumNewSize = buffer_count(Array) + SizeIncrease;
+    u32 NewSize = DoubleCurrentSize > MinimumNewSize ? DoubleCurrentSize : MinimumNewSize;
+    u32 *p = (u32 *) realloc(Array ? buffer_raw(Array) : 0, ItemSize*NewSize + sizeof(u32)*2);
+    if(p)
+    {
+        if(!Array)
+        {
+            p[1] = 0;
+        }
+        p[0] = NewSize;
+
+        Result = p+2;
+    }
+    else
+    {
+        InvalidCodePath;
+    }
+
+    return Result;
+}
 
 #ifdef __cplusplus
 }
